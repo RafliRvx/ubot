3145df2f8edef6ddae31c1034bfc1952943f5f48
RafliRvx <sigitbahtarun123@gmail.com>
2025-10-01 22:44:50 +0700

    Add files via upload

---
+++ PyroUbot/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+MAX_BOT = int(os.getenv("MAX_BOT", "100"))
+
+DEVS = list(map(int, os.getenv("DEVS", "6016943381").split()))
+
+API_ID = int(os.getenv("API_ID", "29984284"))
+
+API_HASH = os.getenv("API_HASH", "c41d118adb5d91352aa6eac8586a3f00")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7253197483:AAEqpze_IMfg4wpKcCrnLzEdD9yorCdAb9I")
+
+OWNER_ID = int(os.getenv("OWNER_ID", "6016943381"))
+
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763").split()))
+
+RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://aortulsk:KxCX5EQdssavL4dm@cluster0.qsywpr2.mongodb.net/")
+
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1003051810060"))
